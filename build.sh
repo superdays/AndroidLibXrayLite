e95c2e19f3e19fdb2909fb9c4d51344eba5e1212
Hiddify <114227601+hiddify1@users.noreply.github.com>
2023-03-28 12:55:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,11 @@ go get -v -d
 go install golang.org/x/mobile/cmd/gomobile@latest
 gomobile init
 gomobile bind -v -androidapi 19 -ldflags='-s -w' ./
+
+cp libv2ray.aar /var/www/html/
+cp libv2ray-sources.jar /var/www/html/
+
+echo "Please download the files:"
+IP=$(curl --connect-timeout 1 -s https://v4.ident.me/)
+echo "http://$IP/libv2ray.aar"
+echo "http://$IP/libv2ray-sources.jar"
